63f6499fe375eed32b3cd84c5a65c90e5bd88227
Ruppikha Shankar <123386373+ruppikha@users.noreply.github.com>
2024-05-31 11:59:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,33 +12,4 @@ public class Main {
             System.out.println("No name provided. This is a test Java file.");
         }
     }
-
-    private static void generateHtmlFile(String name) {
-        String htmlContent = "<!DOCTYPE html>" +
-                "<html lang=\"en\">" +
-                "<head>" +
-                "    <meta charset=\"UTF-8\">" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
-                "    <title>Java Application Output</title>" +
-                "</head>" +
-                "<body>" +
-                "    <h1>Java Application Output</h1>" +
-                "    <div id=\"output\">" +
-                "        <p>Hello, " + name + "!</p>" +
-                "    </div>" +
-                "</body>" +
-                "</html>";
-        
-        try {
-            File docsDir = new File("docs");
-            if (!docsDir.exists()) {
-                docsDir.mkdirs();
-            }
-            try (FileWriter fileWriter = new FileWriter("docs/index.html")) { 
-                fileWriter.write(htmlContent);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
